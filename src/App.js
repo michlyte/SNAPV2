b381ffede7b70a40938e579434bdf4c0b3807d7a
Michlyte <mikefla10@gmail.com>
2017-04-25 14:04:18 +0700

    Firebase

--- src/App.js
+++ src/App.js
@@ -10,29 +10,41 @@ import {SNAPWelcomeNavigator, SNAPExampleNavigator} from './Router';
 import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from 'react-native-fcm';
 
 export default class SampleApp extends Component {
-    componentDidMount() {
-        // FCM.requestPermissions(); // for iOS
-        FCM.getFCMToken().then(token => {
-            console.log(token)
-            // store fcm token in your server
-        });
-        this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
-            // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
-            if (notif.local_notification) {
-                //this is a local notification
-            }
-            if (notif.opened_from_tray) {
-                //app is open/resumed because user clicked banner
-            }
-            // await someAsyncCall();
-            console.log(notif);
+    constructor(props) {
+        super(props);
 
-            if (Platform.OS === 'ios') {
+        this.state = {
+            token: '',
+        }
+    }
+
+    componentDidMount() {
+        FCM.requestPermissions();
+
+        FCM.getFCMToken().then(token => {
+            console.log("TOKEN (getFCMToken)", token);
+            this.setState({ token: token});
+        });
+
+        FCM.getInitialNotification().then(notif => {
+            console.log("INITIAL NOTIFICATION", notif)
+        });
+
+        this.notificationListner = FCM.on(FCMEvent.Notification, notif => {
+            console.log("Notification", notif);
+            if(notif.local_notification){
+                return;
+            }
+            if(notif.opened_from_tray){
+                return;
+            }
+
+            if(Platform.OS ==='ios'){
                 //optional
                 //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
                 //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
                 //notif._notificationType is available for iOS platfrom
-                switch (notif._notificationType) {
+                switch(notif._notificationType){
                     case NotificationType.Remote:
                         notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
                         break;
@@ -44,16 +56,28 @@ export default class SampleApp extends Component {
                         break;
                 }
             }
+            this.showLocalNotification(notif);
         });
-        this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, (token) => {
-            console.log(token)
-            // fcm token may not be available on first load, catch it here
+
+        this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
+            console.log("TOKEN (refreshUnsubscribe)", token);
+            this.setState({ token: token});
+        });
+    }
+
+    showLocalNotification(notif) {
+        FCM.presentLocalNotification({
+            title: notif.title,
+            body: notif.body,
+            priority: "high",
+            click_action: notif.click_action,
+            show_in_foreground: true,
+            local: true
         });
     }
 
     componentWillUnmount() {
-        // stop listening for events
-        this.notificationListener.remove();
+        this.notificationListner.remove();
         this.refreshTokenListener.remove();
     }
 
