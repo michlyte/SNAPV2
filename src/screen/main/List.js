b63786c6cda5fffd441b7c7bc1583093f8d10fc4
Michlyte <mikefla10@gmail.com>
2017-05-15 16:52:52 +0700

    Instagram Item Layout

--- src/screen/main/List.js
+++ src/screen/main/List.js
@@ -1,16 +1,10 @@
-import React, {Component} from 'react';
-import {
-    ListView,
-    FlatList,
-    Text,
-    View,
-    TouchableHighlight,
-} from 'react-native';
-import THEME from '../../util/Theme';
-import CONFIG from '../../util/Config';
-import SCREEN from '../../util/Screen';
+import React, {Component} from "react";
+import {FlatList, Image, Text, TouchableHighlight, View} from "react-native";
+import THEME from "../../util/Theme";
+import CONFIG from "../../util/Config";
+import SCREEN from "../../util/Screen";
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class HomeList extends Component {
     static navigationOptions = ({navigation}) => ({
@@ -32,27 +26,85 @@ export default class HomeList extends Component {
 // Initialize the hardcoded data
     constructor(props) {
         super(props);
-        // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        // this.state = {
-        //     dataSource: ds.cloneWithRows([
-        //         'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-        //     ])
-        // };
     }
 
     _renderItem = ({item}) => {
         return (
-            <Text>{item.title}</Text>
+            <ListItem item={item}/>
         );
     };
 
     render() {
         return (
-            <View style={{flex: 1, paddingTop: 22}}>
+            <View style={{flex: 1, backgroundColor: 'white'}}>
                 <FlatList
-                    data={[{key: 'a', title: 'a title'}, {key: 'b', title: 'b title'}]}
+                    data={[
+                        {key: 'a', title: 'a title'},
+                        {key: 'b', title: 'b title'}
+                    ]}
                     renderItem={this._renderItem}
                 />
+            </View>
+        );
+    }
+}
+
+class ListItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                {/* Bar Header */}
+                <View style={{
+                    flexDirection: 'row',
+                    height: 60,
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    paddingLeft: 15
+                }}>
+
+                    <View style={{flexDirection: 'row', flex: 1}}>
+                        <Image style={{width: 40, height: 40, borderRadius: 20}}
+                               source={{uri: 'http://placehold.it/100x100'}}/>
+                        <Text style={{flex: 1, marginLeft: 10, fontSize: 24}}>{this.props.item.title}</Text>
+                    </View>
+
+                    {/*<Image style={{width: 40, height: 40, borderRadius: 20, marginRight: 20}}*/}
+                    {/*source={{uri: 'http://placehold.it/100x100'}}/>*/}
+                </View>
+
+                {/* Image */}
+
+                <Image style={{height: 280}} source={{uri: 'https://www.snapchat.com/global/social-lg.jpg'}}/>
+
+                {/* Bar Actions */}
+
+                <View style={{flexDirection: 'row', height: 60, alignItems: 'center', paddingLeft: 15}}>
+                    <Image style={{width: 40, height: 40, borderRadius: 20}}
+                           source={{uri: 'http://placehold.it/100x100'}}/>
+                    <Image style={{width: 40, height: 40, borderRadius: 20, marginLeft: 15}}
+                           source={{uri: 'http://placehold.it/100x100'}}/>
+                    <Image style={{width: 40, height: 40, borderRadius: 20, marginLeft: 15}}
+                           source={{uri: 'http://placehold.it/100x100'}}/>
+                </View>
+
+                {/* Bar Description */}
+
+                <View style={{paddingLeft: 15, paddingRight: 15, paddingBottom: 20}}>
+                    <Text numberOfLines={4}>
+                        Death Note, di Indonesia juga dikenal dengan judul Dunia Dewa Kematian, adalah judul sebuah
+                        serial manga Jepang yang ditulis oleh Tsugumi Ohba dan ilustrasi oleh Takeshi Obata. Manga ini
+                        menceritakan tentang Light Yagami, seorang siswa genius yang secara kebetulan menemukan Death
+                        Note milik shinigami (dewa kematian). Direalisasikan di majalah Shonen Jump dari Januari 2004
+                        hingga Mei 2006 dengan total 108 bab. Versi tankoubonnya terbit sebanyak 12 jilid dan 1 jilid
+                        spesial yang berjudul How to Read 13 yang berisi tentang penjelasan dan profil tentang Death
+                        Note. Di Indonesia anime ini ditayangkan oleh SCTV dan Global TV.
+                    </Text>
+                    <Text>15 May 2017</Text>
+                </View>
             </View>
         );
     }
